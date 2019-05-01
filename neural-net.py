0be70f501e93d1348578dcc3f1f016d9b7debc2f
Steven O'Neill <oneillsteven957@gmail.com>
2019-05-01 15:11:48 -0500

    data-set, input variables, and neural net weights

--- neural-net.py
+++ neural-net.py
@@ -10,3 +10,26 @@ def sigmoid(x):
 def sigmoid_to_derivative(output):
     return output * (1 - output)
 
+#training dataset
+int2binary = {}
+binary_dim = 8
+
+largest_number = pow(2, binary_dim)
+binary = np.unpackbits(np.array([range(largest_number)], dtype = np.uint8).T, axis = 1)
+for i in range(largest_number):
+    int2binary[i] = binary[i]
+
+#input variables
+alpha = 0.1
+input_dim = 2
+hidden_dim = 16
+output_dim = 1
+
+#init neural net weights
+synapse_0 = 2*np.random.random((input_dim,hidden_dim)) - 1
+synapse_1 = 2*np.random.random((hidden_dim,output_dim)) - 1
+synapse_h = 2*np.random.random((hidden_dim,hidden_dim)) - 1
+
+synapse_0_update = np.zeros_like(synapse_0)
+synapse_1_update = np.zeros_like(synapse_1)
+synapse_h_update = np.zeros_like(synapse_h)
